701f7e71fa0dc854836a260e58401818db1deb67
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-01-03 13:43:37 -0500

    Add function to validate the name input field. Create message to display if field is empty.

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var phoneError = document.getElementById("phone-error");
 var emailError = document.getElementById("email-error");
 var messageError = document.getElementById("message-error");
 var submitError = document.getElementById("submit-error");
+
+// Function to validate name input field
+function validateName() {
+  var name = document.getElementById("contact-name").value;
+
+  // When name is 0 (nothing entered) 'name is required' displays
+  if (name.length == 0) {
+    nameError.innerHTML = "Name is required";
+  }
+}
