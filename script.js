1051d2418a81cdb65d117024fa47996939dc07c4
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-01-05 11:53:29 -0500

    Add validation for text message. Must be at least 30+ characters long.

--- script.js
+++ script.js
@@ -62,3 +62,17 @@ function validateEmail() {
   emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
   return true;
 }
+
+function validateMessage() {
+  var message = document.getElementById("contact-message").value;
+  var required = 30;
+  var left = required - message.length;
+
+  if (left > 0) {
+    messageError.innerHTML = left + " more characters required";
+    return false;
+  }
+
+  messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+  return true;
+}
